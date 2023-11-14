1df2e9a79e15dacdb87c8d682b52174477fb5085
kimpors <mikita_korshak@ukr.net>
2023-11-14 15:10:11 +0200

    Change dmenu
    apply center patch
    change border size to 2
    change number of lines to 8
    change colorscheme theme to cattpuccin

--- dmenu/config.h
+++ dmenu/config.h
@@ -1,7 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
+static unsigned int border_width = 2;
+static unsigned int lines = 8;
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
@@ -9,12 +13,11 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#cad3f5", "#24273a" },
+	[SchemeSel] = { "#24273a", "#8aadf4" },
+	[SchemeOut] = { "#cad3f5", "#24273a" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -23,4 +26,3 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 0;
