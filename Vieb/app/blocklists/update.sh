d6b77af5e20d163e33060f687f584a9f3b64a26d
kimpors <mikita_korshak@ukr.net>
2023-12-17 13:07:11 +0200

    Remove vieb

--- Vieb/app/blocklists/update.sh
+++
@@ -1,5 +0,0 @@
-cd $(dirname $(realpath $0))
-curl https://easylist-downloads.adblockplus.org/easylist.txt > easylist.txt
-curl https://easylist-downloads.adblockplus.org/easyprivacy.txt > easyprivacy.txt
-curl https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt > ublockfilters.txt
-curl https://raw.githubusercontent.com/ghostery/adblocker/master/packages/adblocker/assets/ublock-origin/resources.txt > resources
