7fc9b04ce7ad5e6012d2631b6fbb2826f4e2cbcf
kimpors <mikita_korshak@ukr.net>
2024-01-10 19:16:47 +0200

    Add icons to slstatus

--- slstatus/config.h
+++ slstatus/config.h
@@ -65,6 +65,6 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-  { datetime, "[%s] ",             "%R" },
-	{ keymap, "[%s]",                NULL },
+  { datetime, " %s ",             "%R" },
+	{ keymap, "  %s",                NULL },
 };
